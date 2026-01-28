e141c4ddb11bd21434ae4a20268d19fb2e7a433e
Timmy6259 <144129012+TimmyJapan@users.noreply.github.com>
2026-01-28 23:45:31 +0900

    B3313 Update?

--- code/src/mupen64plus-core/src/pi/cart_rom.c
+++ code/src/mupen64plus-core/src/pi/cart_rom.c
@@ -25,8 +25,8 @@
 void init_cart_rom(struct cart_rom* cart_rom,
                       uint8_t* rom, size_t rom_size)
 {
-    cart_rom->rom      = rom;
-    cart_rom->rom_size = rom_size;
+//    cart_rom->rom      = rom;
+//    cart_rom->rom_size = rom_size;
 }
 
 void poweron_cart_rom(struct cart_rom* cart_rom)
